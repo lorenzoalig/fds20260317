a4c4e3c8b4fe70c728bd9d11b5aaf6e7db17d996
sofia <animedashpercico@gmail.com>
2026-03-17 22:00:18 -0300

    Update App.java

--- presenca20250317/src/App.java
+++ presenca20250317/src/App.java
@@ -2,9 +2,9 @@ public class App {
     public static void main(String[] args) {
         System.out.println("FUNDAMENTOS DE DESENVOLVIMENTO DE SOFTWARE");
         System.out.println("Presenca da aula 17/3/2026: ");
-
-        // Insira uma linha que mostre o seu nome na tela
-
-
+        
+        // Insira uma linha que mostre o seu nome na tela    
+        // Iohanna Sofia Dietz Maldaner :)
+    
     }
 }
